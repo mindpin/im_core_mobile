230c7f1846b564885baaf1dd967368c724a2240d
lifei <lifei@lifeideMacBook-Pro.local>
2016-12-02 12:11:03 +0800

    add run_android bat sh

---
+++ run_android.bat
@@ -0,0 +1,2 @@
+cd android
+gradlew assembleDebug && adb install -r app\build\outputs\apk\app-debug.apk && cd .. && adb reverse tcp:8081 tcp:8081 && adb reverse tcp:8097 tcp:8097 && start react-native start
