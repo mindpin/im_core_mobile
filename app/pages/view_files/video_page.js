5ff2a3b45aa3922b87804eb3f68c51857fbca7f7
XAINI <shanzhenqiang0814@163.com>
2016-12-16 19:19:36 +0800

    视频播放添加了暂停和进度条

--- app/pages/view_files/video_page.js
+++ app/pages/view_files/video_page.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity
 } from 'react-native';
 
 import Button from 'antd-mobile/lib/button'
@@ -20,20 +21,49 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     flex: 1,
   },
-  backgroundVideo: {
+  fullScreen: {
+    flex: 0,
+    height: 300,
+    marginBottom: 28,
+  },
+  controls: {
+    backgroundColor: "transparent",
+    borderRadius: 5,
     position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+    bottom: 20,
+    left: 20,
+    right: 20,
+  },
+  progress: {
+    flex: 1,
+    flexDirection: 'row',
+    borderRadius: 3,
+    overflow: 'hidden',
+  },
+  innerProgressCompleted: {
+    height: 20,
+    backgroundColor: '#108EE9',
+  },
+  innerProgressRemaining: {
+    height: 20,
+    backgroundColor: '#F3F3F3',
   },
 });
 
 class VideoPage extends BasePage {
   constructor(props) {
     super(props);
+    this.onLoad = this.onLoad.bind(this);
+    this.onProgress = this.onProgress.bind(this);
     this.state = {
       name: "",
+      rate: 1,
+      volume: 1,
+      muted: false,
+      resizeMode: 'contain',
+      repeat: false,
+      duration: 0.0,
+      currentTime: 0.0,
     }
   }
 
@@ -47,18 +77,57 @@ class VideoPage extends BasePage {
     })
   }
 
+  onLoad(data) {
+    this.setState({duration: data.duration});
+  }
+
+  onProgress(data) {
+    this.setState({currentTime: data.currentTime});
+  }
+
   get_loading() {
     return this.refs['loading']
   }
 
+  getCurrentTimePercentage() {
+    if (this.state.currentTime > 0) {
+      return parseFloat(this.state.currentTime) / parseFloat(this.state.duration);
+    } else {
+      return 0;
+    }
+  }
+
   render() {
+    const flexCompleted = this.getCurrentTimePercentage() * 100;
+    const flexRemaining = (1 - this.getCurrentTimePercentage()) * 100;
+
     return (
       <View style={styles.root}>
         <BackNavBar component={this}>{this.state.name}</BackNavBar>
-        <Video
-          resizeMode='cover'
-          source={require('im_core_mobile/app/assets/video/test.mp4')}
-          style={styles.backgroundVideo}/>
+
+        <TouchableOpacity style={styles.fullScreen} onPress={() => {this.setState({paused: !this.state.paused})}}>
+          <Video source={require('im_core_mobile/app/assets/video/test.mp4')}
+            style={styles.fullScreen}
+            rate={this.state.rate}
+            paused={this.state.paused}
+            volume={this.state.volume}
+            muted={this.state.muted}
+            resizeMode={this.state.resizeMode}
+            onLoad={this.onLoad}
+            onProgress={this.onProgress}
+            onEnd={() => {this.setState({repeat: false})}}
+            repeat={this.state.repeat} />
+        </TouchableOpacity>
+
+        <View style={styles.controls}>
+          <View>
+            <View style={styles.progress}>
+              <View style={[styles.innerProgressCompleted, {flex: flexCompleted}]} />
+              <View style={[styles.innerProgressRemaining, {flex: flexRemaining}]} />
+            </View>
+          </View>
+        </View>
+
         <Loading ref={'loading'} />
       </View>
     );
